f0fd43429f80f3102db6a1868f844229eb49bcec
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-02-05 10:57:06 +0100

    used geolocation api to get user location

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+	const postion = postion => {
+		console.log(postion);
+	};
+	const err = err => {
+		console.log(`${err} - something went wrong`);
+	};
+	window.navigator.geolocation.getCurrentPosition(postion, err);
 	return (
 		<div>
 			<h1>Hello World</h1>
